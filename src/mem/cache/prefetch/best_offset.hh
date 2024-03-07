4ed41511cad7242510291b3071eb1b88607f33ec
Øystein Weibell <oystebw@stud.ntnu.no>
2024-03-07 21:21:46 +0100

    misc: starting on structure and member variables

--- src/mem/cache/prefetch/best_offset.hh
+++ src/mem/cache/prefetch/best_offset.hh
@@ -25,6 +25,20 @@ class BestOffsetPrefetcher : public Queued
 {
 
   protected:
+		struct Members {
+			const int SCORE_MAX;
+			const int ROUND_MAX;
+			const int BAD_SCORE;
+			const int RR_SIZE;
+			const int NUMBER_OF_OFFSETS
+
+			bool prefetcherEnabled = false;
+			int bestOffset = 0;
+			int round = 0;
+
+			std::vector<std::pair<int, int>> scores;
+			std::vector<Addr> recentRequests;
+		};
 
   public:
     BestOffsetPrefetcher(const BestOffsetPrefetcherParams &p);
