06e8c1ef145d5a5d39e3f42902ec98385a0383fa
Teloecho <teloecho@posteo.eu>
2024-11-03 17:22:26 +0100

    mem: bo: move members of Members struct out, make log2blockSize const, add implementation specific doc strings

--- src/mem/cache/prefetch/best_offset.cc
+++ src/mem/cache/prefetch/best_offset.cc
@@ -23,7 +23,7 @@ namespace prefetch
 
 
 BestOffsetPrefetcher::BestOffsetPrefetcher(const BestOffsetPrefetcherParams &params)
-    : Queued(params), SCORE_MAX(params.scoreMax), ROUND_MAX(params.roundMax), BAD_SCORE(params.badScore),
+    : Queued(params), SCOREMAX(params.scoreMax), ROUNDMAX(params.roundMax), BADSCORE(params.badScore),
     RR_SIZE(params.rrSize) {}
 
 
@@ -37,30 +37,30 @@ BestOffsetPrefetcher::calculatePrefetch(const PrefetchInfo &pfi,
     }
     Addr tag = (accessAddr >> log2blockSize);
 
-    Addr tagToBeTested = tag - M.offsetScorePair[M.subround].first; // (X - d_i)
+    Addr tagToBeTested = tag - this->offsetScorePair[this->subround].first; // (X - d_i)
     // if this resides in the RR, we increment the corresponding score
-    for(auto& recent: M.recentRequests){
+    for(auto& recent: this->recentRequests){
         if(recent == tagToBeTested){
-            M.offsetScorePair[M.subround].second++;
+            this->offsetScorePair[this->subround].second++;
         }
     }
 
-    Addr bestOffset = M.bestOffset << log2blockSize;
+    Addr bestOffset = this->bestOffset << log2blockSize;
 
     // at the end of a subround, we do the following
-    M.subround++;
-    if(M.subround != M.NUMBER_OF_OFFSETS){ // we don't conclude in the middle of a round
-        if(M.prefetcherEnabled && (accessAddr >> 12) == ((accessAddr + bestOffset) >> 12)){ // ensure that we don't cross page boundries
+    this->subround++;
+    if(this->subround != this->NUMBER_OF_OFFSETS){ // we don't conclude in the middle of a round
+        if(this->prefetcherEnabled && (accessAddr >> 12) == ((accessAddr + bestOffset) >> 12)){ // ensure that we don't cross page boundries
             addresses.push_back(AddrPriority(accessAddr +  bestOffset, 0));
         }
         return;
     }
 
     // we have finished a round and do some book-keeping
-    M.subround = 0;
-    M.round++;
-    if(M.round < SCORE_MAX){ // not possible to reach SCORE_MAX yet
-        if(M.prefetcherEnabled && (accessAddr >> 12) == ((accessAddr + bestOffset) >> 12)){
+    this->subround = 0;
+    this->round++;
+    if(this->round < SCOREMAX){ // not possible to reach SCOREMAX yet
+        if(this->prefetcherEnabled && (accessAddr >> 12) == ((accessAddr + bestOffset) >> 12)){
             addresses.push_back(AddrPriority(accessAddr +  bestOffset, 0));           
         }
         return;
@@ -70,41 +70,41 @@ BestOffsetPrefetcher::calculatePrefetch(const PrefetchInfo &pfi,
     int bestOffset_temp = 0;
     int bestScore_temp = 0;
     // we find the offset that has scored the highest so far
-    for(auto& pair : M.offsetScorePair){
+    for(auto& pair : this->offsetScorePair){
         if(pair.second > bestScore_temp){
             bestScore_temp = pair.second;
             bestOffset_temp = pair.first;
         }
     }
     // and check if the conditions for terminating this training phase are in place
-    if(M.round == ROUND_MAX || bestScore_temp >= SCORE_MAX){
-        M.bestOffset = bestOffset_temp;
-        M.subround = 0;
-        M.round = 0;
-        for(auto& pair : M.offsetScorePair){
+    if(this->round == ROUNDMAX || bestScore_temp >= SCOREMAX){
+        this->bestOffset = bestOffset_temp;
+        this->subround = 0;
+        this->round = 0;
+        for(auto& pair : this->offsetScorePair){
             pair.second = 0;
         }
-        if(M.bestOffset > BAD_SCORE){
-            M.prefetcherEnabled = true;
+        if(this->bestOffset > BADSCORE){
+            this->prefetcherEnabled = true;
         }
         else{
-            M.prefetcherEnabled = false;
+            this->prefetcherEnabled = false;
         }
     }
-    if(M.prefetcherEnabled && (accessAddr >> 12) == ((accessAddr + bestOffset) >> 12)){
+    if(this->prefetcherEnabled && (accessAddr >> 12) == ((accessAddr + bestOffset) >> 12)){
         addresses.push_back(AddrPriority(accessAddr +  bestOffset, 0));       
     }
 }
 
 void BestOffsetPrefetcher::notifyFill(const PacketPtr &pkt){
-    if(!M.prefetcherEnabled){
+    if(!this->prefetcherEnabled){
         Addr tag = (pkt->getAddr() >> log2blockSize);
         // we only care about fills from the L3 that is the result of prefetching
         // making sure that we only store the maximum capacity of RR
-        if(M.recentRequests.size() == RR_SIZE){
-            M.recentRequests.pop_back();
+        if(this->recentRequests.size() == RR_SIZE){
+            this->recentRequests.pop_back();
         }
-        M.recentRequests.push_front(tag);
+        this->recentRequests.push_front(tag);
     }
 }
 
@@ -113,12 +113,12 @@ void BestOffsetPrefetcher::notifyPrefetchFill(const PacketPtr &pkt){
     Addr tag = (pkt->getAddr() >> log2blockSize);
     // we only care about fills from the L3 that is the result of prefetching
     
-    int tagToBeStored = tag - M.bestOffset; // (Y - D) in figure
+    int tagToBeStored = tag - this->bestOffset; // (Y - D) in figure
     // making sure that we only store the maximum capacity of RR
-    if(M.recentRequests.size() == RR_SIZE){
-        M.recentRequests.pop_back();
+    if(this->recentRequests.size() == RR_SIZE){
+        this->recentRequests.pop_back();
     }
-    M.recentRequests.push_front(tagToBeStored);
+    this->recentRequests.push_front(tagToBeStored);
 }
 
 } // namespace prefetch
--- src/mem/cache/prefetch/best_offset.hh
+++ src/mem/cache/prefetch/best_offset.hh
@@ -6,8 +6,9 @@
  * @file
  * Header file for our implementation of a Best Offset Prefetcher following
  * the paper of Pierre Michaud https://inria.hal.science/hal-01254863v1.
- * However the unlimited recent requests table (recentRequests) is one of the
- * simplifications.
+ * However the recent requests table (recentRequests) is simplified as deqeue,
+ * allowing duplicated entries, which diverges from the papers implementation
+ * as cache-like structure, "direct mapped, accessed through a hash function".
  */
 
 #ifndef __MEM_CACHE_PREFETCH_BEST_OFFSET_HH__
@@ -38,44 +39,105 @@ namespace prefetch
 
 class BestOffsetPrefetcher : public Queued
 {
+  private:
+    /**
+     * When an offset reaches this score at the end of a round, it will be the
+     * next bestOffset and the learning phase starts over again.
+     */
+    const int SCOREMAX;
 
-  protected:
-		struct Members {
-			const int NUMBER_OF_OFFSETS = 25;
+    /**
+     * After this number of rounds and if no score reached SCOREMAX, the
+     * learning phase finishes and the highest scoring offset is the next best
+     * offset, if it is also higher than BADSCORE.
+     */
+    const int ROUNDMAX;
 
-			bool prefetcherEnabled = false;
-			int bestOffset = 1;
-			int subround = 0;
-			int round = 0;
+    /**
+     * If a learning phase finishes and no score is above the BADSCORE
+     * threshold, prefetching is switched off.
+     */
+    const int BADSCORE;
 
-			// from the paper
-			std::vector<std::pair<int, int>> offsetScorePair {
-				{1, 0},  {2, 0},  {3, 0},  {4, 0},  {5, 0},  {6, 0},  {8, 0},  {9, 0},
-				{10, 0}, {12, 0}, {15, 0}, {16, 0}, {18, 0}, {20, 0}, {24, 0}, {25, 0},
-				{27, 0}, {30, 0}, {32, 0}, {36, 0}, {40, 0}, {45, 0}, {48, 0}, {50, 0},
-				{54, 0}, {60, 0}
-				};
-			std::deque<int> recentRequests;
-		} M;
-		const int SCORE_MAX;
-		const int ROUND_MAX;
-		const int BAD_SCORE;
-		const int RR_SIZE;
-		int log2blockSize = int(std::log2(Base::blkSize));
+    /**
+     * The maximum size of the RR (recent requests) table. If recentRequests
+     * reached this size, for each new element at the front one old element at
+     * the end is removed.
+     */
+    const int RR_SIZE;
+
+    /** Right shift an address by that value to get the block address (tag). */
+    const int log2blockSize = int(std::log2(Base::blkSize));
+
+    /**
+     * Limit the offsets to the first NUMBER_OF_OFFSETS offsets in
+     * offsetScorePair.
+     */
+    const int NUMBER_OF_OFFSETS = 25;
+
+    /**
+     * If this flag is false, no prefetch requests are issued, the learning
+     * phases however continue. If an offset scorse higher than BADSCORE at
+     * the end of a learning phase, a new bestOffset is found and prefetching
+     * is enabled again.
+     */
+    bool prefetcherEnabled = false;
+
+    /**
+     * The current best offset which is used to issue prefetch requests.
+     */
+    int bestOffset = 1;
+
+    /**
+     * Round counter. A learning phase consists of several rounds. It is
+     * limited by ROUNDMAX.
+     */
+    int round = 0;
+
+    /**
+     * During a round, each offset shall be tested once. This counter is
+     * limited by NUMBER_OF_OFFSETS and serves as an index into
+     * offsetScorePair vector.
+     */
+    int subround = 0;
+
+    /**
+     * The
+     */
+    std::vector<std::pair<int, int>> offsetScorePair {
+        {1, 0},  {2, 0},  {3, 0},  {4, 0},  {5, 0},  {6, 0},  {8, 0},  {9, 0},
+        {10, 0}, {12, 0}, {15, 0}, {16, 0}, {18, 0}, {20, 0}, {24, 0}, {25, 0},
+        {27, 0}, {30, 0}, {32, 0}, {36, 0}, {40, 0}, {45, 0}, {48, 0}, {50, 0},
+        {54, 0}, {60, 0}
+    };
+
+    /**
+     * The RR (recent requests) table. It is filled with the block addresses
+     * (tags) of prefetched blocks as soon as they arrive from L3.
+     */
+    std::deque<int> recentRequests;
 
   public:
     BestOffsetPrefetcher(const BestOffsetPrefetcherParams &p);
 
-	/**
-	 * Calculate which addresses to prefetch according to the best offset
-	 * prefetch algorithm.
-	 */
+    /**
+     * Calculate which addresses to prefetch according to the best offset
+     * prefetch algorithm.
+     */
     void calculatePrefetch(const PrefetchInfo &pfi,
                            std::vector<AddrPriority> &addresses);
-	
-	void notifyFill(const PacketPtr &ptr) override;
-	void notifyPrefetchFill(const PacketPtr &ptr) override;
+    
+    /**
+     * Is invoked on any L2 cache fill. Used to learn offsets while the
+     * prefetcher is disabled.
+     */
+    void notifyFill(const PacketPtr &ptr) override;
 
+    /**
+     * Is invoked only on L2 fills if the block is a prefetched block. Used to
+     * learn offsets while the prefetcher is enabled.
+     */
+    void notifyPrefetchFill(const PacketPtr &ptr) override;
 };
 
 } //namespace prefetch
